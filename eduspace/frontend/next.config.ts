6b4ff8de7883ef1a3c7ab8cd755cffd2b71f4213
huangtianxiang <2539424017@qq.com>
2026-07-17 19:33:57 +0800

    项目初始化，部署前后端环境

---
+++ eduspace/frontend/next.config.ts
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://127.0.0.1:5328/api/:path*',
+      },
+    ]
+  },
+};
+export default nextConfig;
